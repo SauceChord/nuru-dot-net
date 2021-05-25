195f7d9fff17ca743631461c2d8abff9c83f586a
SauceChord <58859889+SauceChord@users.noreply.github.com>
2021-05-25 10:56:08 +0200

    Add CellWriter

---
+++ nuru/nuru.NUI.Tests/Writers/TestCellWriter.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using nuru.NUI.Writers;
+
+namespace nuru.NUI.Tests.Writers
+{
+    public class TestCellWriter : TestReadWriteBase, IGlyphWriter, IColorPairWriter, IMetadataWriter
+    {
+        protected string calls;
+        protected CellWriter cellWriter;
+
+        public override void Setup()
+        {
+            base.Setup();
+            calls = "";
+            cellWriter = new CellWriter(this, this, this);
+        }
+
+        [Test]
+        public void ReadCellCallsInOrder()
+        {
+            cellWriter.Write(new Cell('A', new ColorPair(12, 2), 123));
+            Assert.That(calls, Is.EqualTo("A12, 2123"));
+        }
+
+        void IGlyphWriter.Write(char glyph)
+        {
+            calls += glyph;
+        }
+
+        void IColorPairWriter.Write(ColorPair pair)
+        {
+            calls += pair;
+        }
+
+        void IMetadataWriter.Write(ushort metadata)
+        {
+            calls += metadata;
+        }
+    }
+}
---
+++ nuru/nuru.NUI/Writers/CellWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nuru.NUI.Writers
+{
+    public class CellWriter
+    {
+        protected IGlyphWriter glyph;
+        protected IColorPairWriter color;
+        protected IMetadataWriter metadata;
+
+        public CellWriter(IGlyphWriter glyph, IColorPairWriter color, IMetadataWriter meta)
+        {
+            this.glyph = glyph;
+            this.color = color;
+            this.metadata = meta;
+        }
+
+        public void Write(Cell cell)
+        {
+            glyph.Write(cell.Character);
+            color.Write(cell.Colors);
+            metadata.Write(cell.Metadata);
+        }
+    }
+}

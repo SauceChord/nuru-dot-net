f42336f1c9c070aee6019fb13d5569a89ee0a0f0
SauceChord <58859889+SauceChord@users.noreply.github.com>
2021-05-27 04:58:06 +0200

    Fixed failing test

--- nuru/nuru.Unit.Tests/RGBTests.cs
+++ nuru/nuru.Unit.Tests/RGBTests.cs
@@ -5,7 +5,7 @@ namespace nuru.Unit.Tests
     public class RGBTests
     {
         [TestCase(0x0020ff00, ExpectedResult = "20FF00")]
-        [TestCase(0xff010203, ExpectedResult = "010203")]
+        [TestCase(0x7f010203, ExpectedResult = "010203")]
         public string ToString_ReturnsHexTriple(int rgbInt)
         {
             var rgb = new RGB(rgbInt);

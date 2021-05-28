04a4ab1189808926528a415be92cad24814f36f0
SauceChord <58859889+SauceChord@users.noreply.github.com>
2021-05-28 15:10:05 +0200

    Add PaletteFile class

---
+++ nuru/nuru/IO/PaletteFile.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+
+namespace nuru.IO
+{
+    public enum PaletteType : byte
+    {
+        ANSI8 = 1,
+        Unicode = 2,
+        RGB = 3,
+    }
+
+    public class PaletteFile
+    {
+        public const string Signature = "NURUPAL";
+        public const byte Version = 1;
+        public readonly PaletteType PaletteType;
+        public char KeyChar = ' ';
+        public byte KeyFore = 15;
+        public byte KeyBack = 0;
+        public uint Userdata;
+
+        private const int entryCount = 256;
+        private byte[] entries;
+
+        public PaletteFile(PaletteType paletteType)
+        {
+            switch (paletteType)
+            {
+                case PaletteType.ANSI8:
+                case PaletteType.Unicode:
+                case PaletteType.RGB:
+                    PaletteType = paletteType;
+                    entries = new byte[entryCount * (int)paletteType];
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("paletteType");
+            }
+        }
+
+        public void SetANSI8(byte index, byte ansi8)
+        {
+            if (PaletteType != PaletteType.ANSI8)
+                throw new InvalidOperationException($"Invalid call for PaletteType {PaletteType}");
+
+            entries[index] = ansi8;
+        }
+
+        public byte GetANSI8(byte index)
+        {
+            if (PaletteType != PaletteType.ANSI8)
+                throw new InvalidOperationException($"Invalid call for PaletteType {PaletteType}");
+
+            return entries[index];
+        }
+
+        public void SetChar(byte index, char value)
+        {
+            if (PaletteType != PaletteType.Unicode)
+                throw new InvalidOperationException($"Invalid call for PaletteType {PaletteType}");
+
+            var idx = index * 2;
+            entries[idx] = (byte)(value & 0xff);
+            entries[idx + 1] = (byte)(value >> 8);
+        }
+
+        public char GetChar(byte index)
+        {
+            if (PaletteType != PaletteType.Unicode)
+                throw new InvalidOperationException($"Invalid call for PaletteType {PaletteType}");
+
+            var idx = index * 2;
+            return (char)(entries[idx] | entries[idx + 1] << 8);
+        }
+
+        public void SetRGB(byte index, RGB rgb)
+        {
+            if (PaletteType != PaletteType.RGB)
+                throw new InvalidOperationException($"Invalid call for PaletteType {PaletteType}");
+
+            int idx = index * 3;
+            entries[idx + 0] = rgb.Red;
+            entries[idx + 1] = rgb.Green;
+            entries[idx + 2] = rgb.Blue;
+        }
+
+        public RGB GetRGB(byte index)
+        {
+            if (PaletteType != PaletteType.RGB)
+                throw new InvalidOperationException($"Invalid call for PaletteType {PaletteType}");
+
+            int idx = index * 3;
+            return new RGB(
+                entries[idx + 0],
+                entries[idx + 1],
+                entries[idx + 2]);
+        }
+    }
+}
---
+++ nuru/nuruTests/IO/PaletteFileTests.cs
@@ -0,0 +1,118 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace nuru.IO
+{
+    public class PaletteFileTests
+    {
+        [Test]
+        public void Constructor_GivenValidPaletteType_InitializesFields()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.RGB);
+
+            Assert.That(file.PaletteType, Is.EqualTo(PaletteType.RGB));
+            Assert.That(file.KeyChar, Is.EqualTo(' '));
+            Assert.That(file.KeyFore, Is.EqualTo(15));
+            Assert.That(file.KeyBack, Is.EqualTo(0));
+            Assert.That(file.Userdata, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Constructor_GivenInvalidPaletteType_Throws()
+        {
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(
+                    () => new PaletteFile((PaletteType)255)
+                );
+            Assert.That(exception.ParamName, Is.EqualTo("paletteType"));
+        }
+
+        [Test]
+        public void GetANSI8_GivenCallToSetANSI8_ReturnsSameValue()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.ANSI8);
+            file.SetANSI8(0, 1);
+            file.SetANSI8(255, 2);
+
+            Assert.That(file.GetANSI8(0), Is.EqualTo(1));
+            Assert.That(file.GetANSI8(255), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void GetChar_GivenCallToSetChar_ReturnsSameValue()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.Unicode);
+            file.SetChar(0, 'A');
+            file.SetChar(255, 'B');
+            file.SetChar(1, 'Ɠ');
+
+            Assert.That(file.GetChar(0), Is.EqualTo('A'));
+            Assert.That(file.GetChar(255), Is.EqualTo('B'));
+            Assert.That(file.GetChar(1), Is.EqualTo('Ɠ'));
+        }
+
+        [Test]
+        public void GetRGB_GivenCallToSetRGB_ReturnsSameValue()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.RGB);
+            file.SetRGB(0, new RGB(1, 2, 3));
+            file.SetRGB(255, new RGB(4, 5, 6));
+
+            Assert.That(file.GetRGB(0), Is.EqualTo(new RGB(1, 2, 3)));
+            Assert.That(file.GetRGB(255), Is.EqualTo(new RGB(4, 5, 6)));
+        }
+
+        [Test]
+        public void GetANSI8_GivenNonANSI8PaletteType_Throws()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.Unicode);
+            Assert.Throws<InvalidOperationException>(
+                    () => file.GetANSI8(0)
+                );
+        }
+
+        [Test]
+        public void SetANSI8_GivenNonANSI8PaletteType_Throws()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.Unicode);
+            Assert.Throws<InvalidOperationException>(
+                    () => file.SetANSI8(0, 0)
+                );
+        }
+
+        [Test]
+        public void GetChar_GivenNonUnicodePaletteType_Throws()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.ANSI8);
+            Assert.Throws<InvalidOperationException>(
+                    () => file.GetChar(0)
+                );
+        }
+
+        [Test]
+        public void SetChar_GivenNonUnicodePaletteType_Throws()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.ANSI8);
+            Assert.Throws<InvalidOperationException>(
+                    () => file.SetChar(0, 'A')
+                );
+        }
+
+        [Test]
+        public void GetRGB_GivenNonRGBPaletteType_Throws()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.ANSI8);
+            Assert.Throws<InvalidOperationException>(
+                    () => file.GetRGB(0)
+                );
+        }
+
+        [Test]
+        public void SetRGB_GivenNonRGBPaletteType_Throws()
+        {
+            PaletteFile file = new PaletteFile(PaletteType.ANSI8);
+            Assert.Throws<InvalidOperationException>(
+                    () => file.SetRGB(0, new RGB())
+                );
+        }
+    }
+}
